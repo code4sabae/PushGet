8a937bab117f4a9e2350fe3944b0cfa340da1be4
Taisuke Fukuno <fukuno@jig.jp>
2021-01-10 17:27:16 +0900

    Initial Commit

---
+++ PushGet/PushGetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushGetApp.swift
+//  PushGet
+//
+//  Created by Taisuke Fukuno on 2021/01/10.
+//
+
+import SwiftUI
+
+@main
+struct PushGetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
